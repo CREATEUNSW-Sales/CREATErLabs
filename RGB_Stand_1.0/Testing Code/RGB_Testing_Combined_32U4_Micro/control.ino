ed953e3ba2f0dbc942f8a11158fe3fc978ce7517
Nanqi Chen <nanqi.c@gmail.com>
2022-04-29 21:10:38 +1000

    Code specific for pro micro pins

---
+++ RGB_Stand_1.0/Testing Code/RGB_Testing_Combined_32U4_Micro/control.ino
@@ -0,0 +1,62 @@
+// Use serial monitor to send mode commands
+void serialControl() {
+  // If there is data in the input buffer
+  if (Serial.available() > 0) {
+    // Read the next incoming byte:
+    int incomingByte = Serial.read();
+    if (incomingByte != '\n') {
+      // Change current mode
+      changeMode(incomingByte - '0');
+      Serial.print("Mode is now: ");
+      Serial.println(mode);
+      //Serial.println(incomingByte);
+    }
+  }
+}
+
+// Use button to sequentially switch modes
+void buttonISR() {
+  // Read if button is pressed
+  //bool buttonState = digitalRead(BUTTON_PIN);
+  static unsigned long prevTime = 0;
+  //Serial.println(buttonState);
+
+  // If button is pressed (LOW) and not waiting for switch bouncing (200ms since previous detected press)
+  if (millis() - prevTime >= 200) {
+    changeMode(mode + 1);
+
+    prevTime = millis();
+  }
+}
+
+void changeMode(int newMode) {
+  if (mode != newMode) {
+    // Use direct port addressing to customise Timer1 and Timer2 for modes 4 and 5
+    if ((mode == 4 || mode == 5) && !(newMode == 4 || newMode == 5)) {
+      disableCustomTimers();
+    } else if (newMode == 4 || newMode == 5) {
+      setupCustomTimers();
+    }
+    //Serial.println(mode);
+    mode = newMode;
+    // Set mode back to 0 once we cycle through all modes
+    mode %= NUM_MODES;
+    // Write new mode to EEPROM (non-volatile memory)
+    EEPROM.write(addr, mode);
+  }
+}
+
+// Read potentiometer and update brightness
+void readPot() {
+  // Read analog resistance of potentiometer
+  int sensorVal = analogRead(A1);
+  // Map to brightness value from 0 - 1023 to 0.0 - 1.0
+  brightness = map(sensorVal, 0, 1023, 0, 1000) / 1000.0;
+}
+
+// Print current RGB values for debugging
+void printRGB() {
+  char buff[20];
+  sprintf(buff,"R: %d, G: %d, B: %d",rgb[0],rgb[1],rgb[2]);
+  Serial.println(buff);
+}
---
+++ RGB_Stand_1.0/Testing Code/RGB_Testing_Combined_32U4_Micro/modes.ino
@@ -0,0 +1,137 @@
+// Set all LEDs to the same singular colour
+void blueSolid() {
+  // Convert HSV (226, 1.0, 1.0) to RGB and write to corresponding pins
+  HsvToRgb(226, 1, brightness, rgb);
+  // Need to subtract from 255 as LEDs are reversed due to being common anode
+  analogWrite(R_PIN, 255 - rgb[0]);
+  analogWrite(G_PIN, 255 - rgb[1]);
+  analogWrite(B_PIN, 255 - rgb[2]);
+
+  // Cycle through LEDs
+  switchLEDs();
+}
+
+void whiteSolid() {
+  // Convert HSV (0, 0, 1.0) to RGB and write to corresponding pins
+  HsvToRgb(0, 0, brightness, rgb);
+  // Need to subtract from 255 as LEDs are reversed due to being common anode
+  analogWrite(R_PIN, 255 - rgb[0]);
+  analogWrite(G_PIN, 255 - rgb[1]);
+  analogWrite(B_PIN, 255 - rgb[2]);
+
+  // Cycle through LEDs
+  switchLEDs();
+}
+
+void orangeSolid() {
+  // Convert HSV (24, 1.0, 1.0) to RGB and write to corresponding pins
+  HsvToRgb(24, 1, brightness, rgb);
+  // Need to subtract from 255 as LEDs are reversed due to being common anode
+  analogWrite(R_PIN, 255 - rgb[0]);
+  analogWrite(G_PIN, 255 - rgb[1]);
+  analogWrite(B_PIN, 255 - rgb[2]);
+
+  // Cycle through LEDs
+  switchLEDs();
+}
+
+
+// Fade all LEDs with the same colours
+void fadeAll() {
+  static int hue = 0;
+  static unsigned long currTime;
+  static unsigned long prevTimeFade = 0;
+
+  currTime = millis();
+
+  // Cycle through hue at rate defined by FADE_INTERVAL (ms)
+  if (currTime - prevTimeFade >= FADE_INTERVAL) {
+    prevTimeFade = currTime;
+    
+    if (hue >= 360) {
+      hue = 0;
+    }
+
+    // Convert HSV (0, 1.0, 1.0) to RGB and write to corresponding pins
+    HsvToRgb(hue, 1, brightness, rgb);
+    // Need to subtract from 255 as LEDs are reversed due to being common anode
+    analogWrite(R_PIN, 255 - rgb[0]);
+    analogWrite(G_PIN, 255 - rgb[1]);
+    analogWrite(B_PIN, 255 - rgb[2]);
+
+    hue++;
+  }
+
+  // Cycle through LEDs
+  switchLEDs();
+}
+
+
+// Set all LEDs to different colours
+void multiSolid() {
+  // Convert HSV (226, 1.0, 1.0) to RGB and write to corresponding pins
+  HsvToRgb(226, 1, brightness, rgb);
+  // No need to subtract as timer already setup to account for common anode
+  // Timer1 Overflow ISR handles LED switching for us using defined arrays of colours
+  colours[0][0] = rgb[0];
+  colours[0][1] = rgb[1];
+  colours[0][2] = rgb[2];
+  HsvToRgb(0, 0, brightness, rgb);
+  colours[1][0] = rgb[0];
+  colours[1][1] = rgb[1];
+  colours[1][2] = rgb[2];
+  HsvToRgb(24, 1, brightness, rgb);
+  colours[2][0] = rgb[0];
+  colours[2][1] = rgb[1];
+  colours[2][2] = rgb[2];
+}
+
+// Fade all LEDs with different colours
+void multiFade() {
+  // Keep track of each hue for each LED
+  static int hue0 = 0;
+  static int hue1 = 120;
+  static int hue2 = 240;
+  
+  if (hue0 >= 360) {
+    hue0 = 0;
+  }
+  if (hue1 >= 360) {
+    hue1 = 0;
+  }
+  if (hue2 >= 360) {
+    hue2 = 0;
+  }
+
+  // Convert HSV (226, 1.0, 1.0) to RGB and write to corresponding pins
+  HsvToRgb(hue0, 1, brightness, rgb);
+  // No need to subtract as timer already setup to account for common anode
+  // Timer1 Overflow ISR handles LED switching for us using defined arrays of colours
+  colours[0][0] = rgb[0];
+  colours[0][1] = rgb[1];
+  colours[0][2] = rgb[2];
+  HsvToRgb(hue1, 1, brightness, rgb);
+  colours[1][0] = rgb[0];
+  colours[1][1] = rgb[1];
+  colours[1][2] = rgb[2];
+  HsvToRgb(hue2, 1, brightness, rgb);
+  colours[2][0] = rgb[0];
+  colours[2][1] = rgb[1];
+  colours[2][2] = rgb[2];
+
+  hue0++;
+  hue1++;
+  hue2++;
+
+  // Cycle through hue at rate defined by FADE_INTERVAL (ms)
+  _delay_ms(FADE_INTERVAL);
+}
+
+
+// Turn all LEDs off
+void off() {
+  // Turn all LEDs off
+  digitalWrite(ANODE_1, LOW);
+  digitalWrite(ANODE_2, LOW);
+  digitalWrite(ANODE_3, LOW);
+}
